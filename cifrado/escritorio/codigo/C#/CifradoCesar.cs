023b00c6911f1c9b81f58dd1683832cbf2d6eb02
elahito <elahigironcueto@gmail.com>
2018-12-15 13:54:05 -0600

    agregado c#

---
+++ cifrado/escritorio/codigo/C#/CifradoCesar.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCipherDecipher
+{
+    class CaeserCipher : ICipherDecipher
+    {
+        public string Cipher(params object[] arguments)
+        {
+            if ( arguments.Length == 2 )
+            {
+                string text = arguments[0] as string;
+                int shift = (int)arguments[1];
+
+                return Cipher(text, shift);
+            }
+
+            return string.Empty;
+        }
+
+        public string Decipher(params object[] arguments)
+        {
+            if (arguments.Length == 2)
+            {
+                string text = arguments[0] as string;
+                int shift = (int)arguments[1];
+
+                return Decipher(text, shift);
+            }
+
+            return string.Empty;
+        }
+
+        private string Cipher( string text, int shift )
+        {
+            StringBuilder result = new StringBuilder();
+
+            string alphabet = "0123456789abcdefghijklmnopqrstuvxywzABCDEFGHIJKLMNOPQRSTUWVXYZ !";
+            int offset = alphabet.Length;
+
+            foreach( char c in text )
+            {
+                char oldChar = c;
+                int oldCharIdx = alphabet.IndexOf(c);
+
+                int newCharIdx = (oldCharIdx + shift + offset) % offset;
+
+                char newChar = alphabet[newCharIdx];
+
+                result.Append(newChar);
+            }
+
+            return result.ToString();
+        }
+
+        private string Decipher( string text, int shift )
+        {
+            return Cipher(text, shift * -1);
+        }
+    }
+}
---
+++ cifrado/escritorio/codigo/C#/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextCipherDecipher
+{
+    public partial class Form1 : Form
+    {
+        private ICipherDecipher myCipher;
+
+        public Form1()
+        {
+            myCipher = new CaeserCipher();
+            //myCipher = new MyCipher();
+
+            InitializeComponent();
+        }
+
+        private void btn_ClearUncipheredText_Click(object sender, EventArgs e)
+        {
+            txtb_UncipheredText.Text = string.Empty;
+        }
+
+        private void btn_ClearCipheredText_Click(object sender, EventArgs e)
+        {
+            txtb_CipheredText.Clear();
+        }
+
+        private void btn_Cipher_Click(object sender, EventArgs e)
+        {
+            string textToBeCiphered = txtb_UncipheredText.Text;
+
+            string textCiphered = myCipher.Cipher(textToBeCiphered, 6);
+
+            txtb_CipheredText.Text = textCiphered;
+        }
+
+        private void btn_Decipher_Click(object sender, EventArgs e)
+        {
+            string textToBeUnciphered = txtb_CipheredText.Text;
+
+            string textUnciphered = myCipher.Decipher(textToBeUnciphered, 6);
+
+            txtb_UncipheredText.Text = textUnciphered;
+        }
+    }
+}
